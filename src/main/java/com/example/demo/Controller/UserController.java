4be9c1155785a4561e14c02295a4226e8f3f1190
akhmet00 <akhmet00@mail.ru>
2020-07-20 14:31:12 +0600

    shop project v2.2

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -45,9 +46,13 @@ public class UserController {
 
     @PostMapping("/register/new")
     public String registerUserRequest(@ModelAttribute("users") Users users,
-                                      BindingResult bindingResult)
+                                      BindingResult bindingResult,
+                                      Model model,
+                                      @RequestParam("userName") String userName)
     {
-        if(bindingResult.hasErrors()){
+        Users user = userService.findByUserName(userName);
+        if(bindingResult.hasErrors() || user!=null){
+            model.addAttribute("message", "This user already exists!");
             return "register";
         }
         userService.save(users);
